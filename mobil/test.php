e6aeff5aa120179fa95271d4517ea50ba36a3bf2
Onur Osman Gule <onurgule@gmail.com>
2020-12-31 01:07:13 +0300

    Delete test.php

--- mobil/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-try {
-     $db = new PDO("mysql:host=localhost;dbname=isimlink_pho", "isimlink_upho", "U{SQRKXRbFA#");
-	 $db->exec("SET NAMES 'utf8'; SET CHARSET 'utf8'");
-} catch ( PDOException $e ){
-     print $e->getMessage();
-}
-?>
